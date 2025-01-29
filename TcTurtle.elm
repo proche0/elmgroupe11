d256b252ef03d1c109b71c476f0be18abac1fd84
Neiluj35 <julien35.brelivet@gmail.com>
2025-01-29 10:35:04 +0100

    Delete TcTurtle.elm

--- TcTurtle.elm
+++
@@ -1,9 +0,0 @@
-module TcTurtle exposing (Instruction(..), Program)
-
-type Instruction
-    = Forward Int
-    | Left Int
-    | Right Int
-    | Repeat Int (List Instruction)
-
-type alias Program = List Instruction
